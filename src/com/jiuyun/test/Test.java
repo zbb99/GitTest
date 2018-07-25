28780161f514f31f132f2754699b88a85d298076
zbb99 <41665507+zbb99@users.noreply.github.com>
2018-07-25 14:48:55 +0800

    Revert "Initial commit"
    This reverts commit 34a26773fffcf9c0eab213bb9d1041e233165307.

--- src/com/jiuyun/test/Test.java
+++
@@ -1,7 +0,0 @@
-package com.jiuyun.test;
-
-public class Test {
-	public static void main(String[] args) {
-		System.out.println("¹þ¹þ¹þ");
-	}
-}
--- src/com/jiuyun/test/Test2.java
+++
@@ -1,7 +0,0 @@
-package com.jiuyun.test;
-
-public class Test2 {
-	public static void main(String[] args) {
-		System.out.println("บวบว");
-	}
-}
